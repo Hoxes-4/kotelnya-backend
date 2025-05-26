7cdbaa36d1f419b29807612bf65d9fa4717d6d93
Hoxes-4 <crazyfuun@mail.ru>
2025-05-26 22:19:58 +1000

    add new rout invitations.js

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
-
+const invitationsRouter = require('./routes/invitations');
 const cors = require('cors');
 app.use(cors());
 
 
 dotenv.config();
 app.use(express.json());
-
 app.use('/api/boards', require('./routes/boards'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
@@ -19,6 +18,7 @@ app.use('/api', require('./routes/tasks'));
 app.use('/api', require('./routes/columns'));
 app.use('/api', require('./routes/notes'));
 app.use('/uploads', express.static('uploads'));
+app.use('/api/invitations', invitationsRouter);
 
 app.get('/', (req, res) => {
     res.send('✅ Сервер работает! Добро пожаловать в API');
--- routes/invitations.js
+++ routes/invitations.js
@@ -9,3 +9,22 @@ const {
   deleteInvitation,
 } = require('../controllers/invitationController');
 const auth = require('../middlewares/authMiddleware');
+
+router.use(auth);
+
+// POST /api/invitations/send - Отправить приглашение в проект
+router.post('/send', sendInvitation);
+
+// GET /api/invitations - Получить все приглашения текущего пользователя
+router.get('/', getInvitations);
+
+// POST /api/invitations/accept - Принять приглашение
+router.post('/accept', acceptInvitation);
+
+// POST /api/invitations/reject - Отклонить приглашение
+router.post('/reject', rejectInvitation);
+
+// DELETE /api/invitations/:invitationId - Удалить приглашение (отправителем или админом/владельцем)
+router.delete('/:invitationId', deleteInvitation);
+
+module.exports = router;
